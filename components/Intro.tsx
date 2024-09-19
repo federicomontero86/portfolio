34ebba817363760a85f804ae0af6daf79a02ca7a
Federico Montero <federicomontero86@gmail.com>
2024-09-19 14:41:13 -0300

    update Intro info

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -60,7 +60,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
       >
         <span className="font-bold">Hello, I'm Federico.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> from{" "}
+        <span className="font-bold">front-end developer</span> from{" "}
         <span className="font-bold">Montevideo, Uruguay</span>. I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
