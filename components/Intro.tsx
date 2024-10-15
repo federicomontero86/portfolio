8e132779ca23956f9707ef69519544f37f40f2ad
Federico Montero <federicomontero86@gmail.com>
2024-10-14 21:50:12 -0300

    updating Intro h1

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -55,7 +55,7 @@ export default function Intro() {
         </div>
       </div>
       <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 md:px-[3.7rem] text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
