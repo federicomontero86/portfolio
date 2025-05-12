0361bb73ce7dac14517da470f6ed2110a566f39f
Federico Montero <federicomontero86@gmail.com>
2025-05-12 16:09:33 -0300

    deleting comments in layout.tsx, Experience.tsx and hooks.ts

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -19,48 +19,6 @@ export default function Experience() {
   const { theme } = useThemeContext();
 
   return (
-    // <section ref={ref} id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
-    //   <SectionHeading>My experience</SectionHeading>
-    //   <VerticalTimeline lineColor="">
-    //     {experiencesData.map((item, index) => (
-    //       <React.Fragment key={index}>
-    //         <VerticalTimelineElement
-    //           contentStyle={{
-    //             background:
-    //               theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
-    //             boxShadow: "none",
-    //             border: "1px solid rgba(0, 0, 0, 0.05)",
-    //             textAlign: "left",
-    //             padding: "1.3rem 2rem",
-    //           }}
-    //           contentArrowStyle={{
-    //             borderRight:
-    //               theme === "light"
-    //                 ? "0.4rem solid #9ca3af"
-    //                 : "0.4rem solid rgba(255, 255, 255, 0.5)",
-    //           }}
-    //           date={item.date}
-    //           icon={item.icon}
-    //           iconStyle={{
-    //             background: theme === "light" ? "white" : "#1D2432",
-    //             fontSize: "1.5rem",
-    //             opacity: "1",
-    //           }}
-    //           visible={true}
-    //         >
-    //           <h3 className="font-semibold capitalize">{item.title}</h3>
-    //           <p className="font-normal !mt-0">{item.location}</p>
-    //           <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
-    //             {item.description}
-    //           </p>
-    //           <p className="!mt-1 !font-normal !text-sm italic text-gray-700 dark:text-white/75">
-    //             {item.techs}
-    //           </p>
-    //         </VerticalTimelineElement>
-    //       </React.Fragment>
-    //     ))}
-    //   </VerticalTimeline>
-    // </section>
     <section
       ref={ref}
       id="experience"
--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,24 +1,3 @@
-// import { useEffect } from "react";
-// import { useInView } from "react-intersection-observer";
-
-// import { useActiveSectionContext } from "@/context/ActiveSectionContext";
-// import type { SectionName } from "./types";
-
-// export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
-//   const { ref, inView } = useInView({ threshold });
-//   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
-
-//   useEffect(
-//     function () {
-//       if (inView && Date.now() - timeOfLastClick > 1000)
-//         setActiveSection(sectionName);
-//     },
-//     [inView, setActiveSection, timeOfLastClick, sectionName]
-//   );
-
-//   return { ref };
-// }
-
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
