2ee8ae450b4d048644d1329fc3d8b5e8d510f21e
Federico Montero <federicomontero86@gmail.com>
2025-05-12 15:54:35 -0300

    fixing header active section by scrolling up through experience

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,17 +1,39 @@
+// import { useEffect } from "react";
+// import { useInView } from "react-intersection-observer";
+
+// import { useActiveSectionContext } from "@/context/ActiveSectionContext";
+// import type { SectionName } from "./types";
+
+// export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+//   const { ref, inView } = useInView({ threshold });
+//   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
+
+//   useEffect(
+//     function () {
+//       if (inView && Date.now() - timeOfLastClick > 1000)
+//         setActiveSection(sectionName);
+//     },
+//     [inView, setActiveSection, timeOfLastClick, sectionName]
+//   );
+
+//   return { ref };
+// }
+
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
 import { useActiveSectionContext } from "@/context/ActiveSectionContext";
 import type { SectionName } from "./types";
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
-  const { ref, inView } = useInView({ threshold });
+export function useSectionInView(sectionName: SectionName, threshold = 0.25) {
+  const { ref, inView } = useInView({ threshold, triggerOnce: false }); // Detecta en ambas direcciones
   const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(
     function () {
-      if (inView && Date.now() - timeOfLastClick > 1000)
+      if (inView && Date.now() - timeOfLastClick > 1000) {
         setActiveSection(sectionName);
+      }
     },
     [inView, setActiveSection, timeOfLastClick, sectionName]
   );
